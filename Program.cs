863009d29f62951735e0bc22f9cb0531ae436c7d
Harikrishna415 <103161744+Harikrishna415@users.noreply.github.com>
2022-04-07 08:40:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<employee> employees = new List<employee>();
+            employees.Add(new employee() { name = "Anurag", gender = 0 });
+            employees.Add(new employee() { name = "Pranaya", gender = 1 });
+            employees.Add(new employee() { name = "Priyanka", gender = 2 });
+            employees.Add(new employee() { name = "Sambit", gender = 3 });
+            foreach(var emp in employees)
+            {
+                Console.WriteLine("Name = {0} && Gender = {1}", emp.name, GetGender(emp.gender));
+            }
+            Console.ReadLine();
+
+        }
+        public static string GetGender(int gender)
+        {
+            switch(gender)
+            {
+                case 0:
+                    return "un known";
+                case 1:
+                    return " male";
+                case 2:
+                    return "female";
+                default:
+                    return " Invalid Data for gender";
+            }
+
+        }
+    }
+    public class employee
+    {
+        public string name { get; set; }
+        public int gender { get; set; }
+    }
+}
